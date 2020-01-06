1029d3ec902613e8d50936e4071ab866e863bbe4
pprzychodzen <przychu@gmail.com>
2020-01-06 15:28:58 +0100

    Recipe models updated: - added fields to cover time of creation and modyfication; - picture of dish to upload; - place to put recipe instruction

--- mysite/recipe/models.py
+++ mysite/recipe/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
-    active = models.BooleanField(null=True)
-    title = models.CharField(max_length=300)
+    title = models.CharField(max_length=240)
+    # ingredients = models.
+
+    content = models.TextField(null=True, blank=True)
+    picture = models.FileField(upload_to='media/', blank=True)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+    date_last_modified = models.DateTimeField(auto_now=True)
