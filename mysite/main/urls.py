86130bd2a53378e8fe65355e9f547e29e65debc8
Paweł Przychodzeń <przychu@gmail.com>
2020-01-29 12:02:40 +0100

    rebuilded user login and register
    to fix:
    help text for pass

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -14,16 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path("register/", views.register, name="register"),
-    path("logout", views.logout_request, name='logout'),
-    path("login", views.login_request, name='login'),
+    path('', include('user.urls')),
 
     path("<single_slug>", views.single_slug, name="single_slug"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Tutorial, TutorialCategory, TutorialSeries
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
-from .forms import NewUserForm
 
 
 def single_slug(request, single_slug):
@@ -33,51 +30,3 @@ def homepage(request):
     return render(request=request,
                   template_name="main/categories.html",
                   context={"categories": TutorialCategory.objects.all})
-
-
-def register(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"new account created: {username}")
-            login(request, user)
-            messages.info(request, f"you are now logged in as {username}")
-
-            return redirect("main:homepage")
-        else:
-            for msg in form.error_messages:
-                messages.error(request, f"{msg}:{form.error_messages[msg]}")
-
-    form = NewUserForm
-    return render(request,
-                  "main/register.html",
-                  context={"form": form})
-
-
-def logout_request(request):
-    logout(request)
-    messages.info(request, "logged out successfully!")
-    return redirect("main:homepage")
-
-
-def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"you are now logged in as {username}")
-                return redirect("main:homepage")
-            else:
-                messages.error(request, "invalid username or password")
-        else:
-            messages.error(request, "invalid username or password")
-    form = AuthenticationForm()
-    return render(request,
-                  "main/login.html",
-                  {"form": form})
